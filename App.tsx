3191bde99f0b84d0ae0510ffb9c29a9a358839eb
Marcos Willian <markusuuuu@gmail.com>
2023-10-21 22:38:42 -0300

    feat: realmDB configured

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { AppProvider, UserProvider } from '@realm/react'
 
 import theme from './src/theme';
+import { REALM_APP_ID } from '@env';
 
 import { Loading } from './src/components/Loading';
 
 import { SignIn } from './src/screens/SignIn';
-import { StatusBar } from 'react-native';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -18,14 +21,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar 
-        barStyle="light-content" 
-        backgroundColor="transparent" 
-        translucent 
-      />
-
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar 
+          barStyle="light-content" 
+          backgroundColor="transparent" 
+          translucent 
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
